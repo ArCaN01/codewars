1ff3b66f39b749fddee95cf9198bbfae8f72feb0
ArCaN01 <tga977@outlook.com>
2022-08-24 00:52:07 +0300

    Create 4kyu_snail.py
    1st kata solution on codewars(4 kyu, "Snail")

---
+++ 4kyu_snail.py
@@ -0,0 +1,26 @@
+def snail(snail_map):
+    results = []
+
+
+    while len(snail_map) > 0:
+        results += snail_map[0]
+        del snail_map[0]
+        
+
+        if len(snail_map) > 0:
+            for i in snail_map:
+                results += [i[-1]]
+                del i[-1]
+
+
+            if snail_map[-1]:
+                results += snail_map[-1][::-1]
+                del snail_map[-1]
+
+
+            for i in reversed(snail_map):
+                results += [i[0]]
+                del i[0]
+
+
+    return results
